319346ea02c3ad9484cf00f6e5d43019f6cea850
Thiago André De Almeida <milladm03@millgest.com.br>
2016-08-24 17:38:32 -0300

    Custom EventArgs

--- DelegateAndEvents/Program.cs
+++ DelegateAndEvents/Program.cs
@@ -1,27 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DelegateAndEvents
 {
 
-    public delegate int WorkerPerformedHandler(int hours, WorkType worktpe);
+    public delegate int WorkerPerformedHandler(int hours, WorkType worktype);
     class Program
     {
         static void Main(string[] args)
         {
-            WorkerPerformedHandler delegate1 = new WorkerPerformedHandler(WorkerPerformed1);
-            WorkerPerformedHandler delegate2 = new WorkerPerformedHandler(WorkerPerformed2);
-            WorkerPerformedHandler delegate3 = new WorkerPerformedHandler(WorkerPerformed3);
+            var worker = new Worker();
+//            DoWork(worker.DoWork(3, WorkType.GoToMeetings));
+            
+/*          WorkerPerformedHandler delegate1 = new WorkerPerformedHandler(WorkerPerformed1);
+          WorkerPerformedHandler delegate2 = new WorkerPerformedHandler(WorkerPerformed2);
+          WorkerPerformedHandler delegate3 = new WorkerPerformedHandler(WorkerPerformed3);
 
-            delegate1 += delegate2 + delegate3;
+          delegate1 += delegate2 + delegate3;
 
-            int finalHours = delegate1(7,WorkType.GoToMeetings);
-            Console.WriteLine("Final Hours {0}", finalHours.ToString());
-
-            Console.Read();
+          int finalHours = delegate1(7,WorkType.GoToMeetings);
+          Console.WriteLine("Final Hours {0}", finalHours.ToString());
+*/
+          Console.Read();
         }
 
         static void DoWork(WorkerPerformedHandler del)
---
+++ DelegateAndEvents/WorkPerformedEventArgs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateAndEvents
+{
+    public class WorkPerformedEventArgs : EventArgs
+    {
+        public WorkPerformedEventArgs(int hours, WorkType worktype)
+        {
+            Hours = hours;
+            WorkType = worktype;
+        }
+
+        public int Hours { get; private set; }
+        public WorkType WorkType { get; private set; }
+    }
+}
